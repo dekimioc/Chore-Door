95154c8d5fc05dc6481e17d035d5629681d3f2be
dekimioc <dekimioc@yahoo.com>
2018-10-15 00:55:55 +0200

    added functionality

--- script.js
+++ script.js
@@ -15,5 +15,95 @@ let currentlyPlaying = true;
 
 const startButton = document.getElementById('start');
 
+const playDoor = (door) => {
+    numClosedDoor--;
+    if ( numClosedDoor === 0 ) {
+      gameOver('win');
+    } else if (isBot(door)) {
+      gameOver();
+    }
+  };
+  
+  const isClicked = (door) => {
+    if( door.src === closedDoorPath ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  const isBot = (door) => {
+    if( door.src === botDoorPath) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  const randomChoreDoorGenerator = () => {
+    const choreDoor = Math.floor(Math.random() * numClosedDoor);
+    if( choreDoor === 0) {
+      openDoor1 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+    } else if (choreDoor === 1) {
+      openDoor2 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+    } else if (choreDoor === 2){
+      openDoor3 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor2 = spaceDoorPath;
+    }
+  };
+  
+  door1.onclick = () => {
+    if(currentlyPlaying && !isClicked(doorImage1)) {
+      doorImage1.src = openDoor1;
+      playDoor(door1);
+    }
+  };
+  
+  door2.onclick = () => {
+    if(currentlyPlaying && !isClicked(doorImage2)) {
+      doorImage2.src = openDoor2;
+      playDoor(door2);
+    }
+  };
+  
+  door3.onclick = () => {
+    if(currentlyPlaying && !isClicked(doorImage3)) {
+      doorImage3.src = openDoor3;
+       playDoor(door3);
+    };
+  };
+  
+  startButton.onclick = () => {
+    if ( !currentlyPlaying ) {
+           startRound();
+    }
+  }
+  
+  const startRound = () => {
+    numClosedDoor = 3;
+    doorImage1.src = closedDoorPath;
+    doorImage2.src = closedDoorPath;
+    doorImage3.src = closedDoorPath;
+    startButton.innerHTML = "Good luck!";
+    currentlyPlaying = true;
+    randomChoreDoorGenerator();
+  }
+  
+  const gameOver = (status) => {
+    if(status === 'win') {
+      startButton.innerHTML = "You Win! Play again?"
+    } else {
+      startButton.innerHTML = "Game Over! Play again?"
+    }
+    currentlyPlaying = false;
+  }
+  
+  startRound();
+
 
 
